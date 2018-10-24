d1d66121081466af87aa54451d2816bd541f0c7f
alemtani <alemtani@gmail.com>
2018-10-24 15:36:10 -0700

    Alex tested autonomous code

---
+++ TeamCode/src/main/java/Turtlecode/AlexAutonomous.java
@@ -0,0 +1,48 @@
+package Turtlecode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static Turtlecode.AutonomousConfig.*;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="AlexAutonomous", group ="AutonomousCode")
+public class AlexAutonomous extends LinearOpMode {
+    /**
+     * Team 12547 robot variables
+     */
+
+    /* Declare OpMode members. */
+    private HardwarePushturtl robot = new HardwarePushturtl();   // Use a Pushbot's hardware
+
+    private EncoderDriver encoderDriver = new EncoderDriver(this, robot, telemetry);
+    private PID_Shell pid_shell = new PID_Shell(this, robot, telemetry);
+
+    @Override
+    public void runOpMode() {
+        // Initialize the robot
+        initRobot();
+
+        robot.frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.rearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.rearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        waitForStart();
+
+        //pid_shell.pidLoop(0, 3, 100);
+        encoderDriver.encoderDrive(0.7, 84, 84, 84, 84, 100);
+        encoderDriver.encoderDrive(0.7, -33, 33, -33, 33, 100);
+        encoderDriver.encoderDrive(0.7, 90, 90, 90, 90, 100);
+
+        telemetry.addData("Mission ", "Complete");
+        telemetry.update();
+    }
+
+    private void initRobot() {
+        encoderDriver.init();
+    }
+}
